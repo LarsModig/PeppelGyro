7ce84e57866f1fa2f60381b88be1695b290f1cdc
LarsModig <larswillymodig@gmail.com>
2015-12-29 19:39:03 +0100

    Side-wheel instead of pointing pin

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #define TIMER_INTERVAL_MS 50
 #define PI 3.14159265359
 
+#define DEBUG 1
+
 
 
 Window *s_main_window;
@@ -17,8 +19,8 @@ int16_t counter = 0;
 
 
 int32_t rotator() {
-  counter = counter + 1 % 365;
-  int32_t toreturn = (TRIG_MAX_ANGLE * counter / 365);
+  counter = (counter + 1) % 365;
+  int32_t toreturn = TRIG_MAX_ANGLE * counter / 365;
   return toreturn;
 }
 
@@ -28,33 +30,74 @@ int get_compas_heading() {
   return data.magnetic_heading;
 }
 
-GPoint get_compas_pointer_coords(GPoint center) {
+GPoint get_compas_pointer_coords(GPoint center, int angle) {
+  //not used, could have use later
+  
   #define LENGTH_OF_ARROW 70
-
-  int angle = rotator();
   
   GPoint g;
   g.x = (sin_lookup(angle) * LENGTH_OF_ARROW / TRIG_MAX_RATIO) + center.x;
   g.y = (-cos_lookup(angle) * LENGTH_OF_ARROW / TRIG_MAX_RATIO) + center.y;
-  
-  snprintf(debug1, sizeof(debug1), "x: %d", (int) (sin_lookup(angle) * LENGTH_OF_ARROW / TRIG_MAX_RATIO));
-  snprintf(debug2, sizeof(debug1), "y: %d", (int) (-cos_lookup(angle) * LENGTH_OF_ARROW / TRIG_MAX_RATIO));
-  
+    
   return g;
 }
 
-static void canvas_update_proc(Layer *layer, GContext *ctx) {
-  GPoint center = GPoint(window_bounds.size.w/2, window_bounds.size.h/2);
+void draw_disc_pies(GContext *ctx, int trig_angle, int8_t piesize, int radius, GPoint center, GColor c1, GColor c2, bool draw_outline) {
   
-  GPoint outer = get_compas_pointer_coords(center);
+  int offset = trig_angle % (TRIG_MAX_ANGLE * piesize / 360);
+
+  int coloroffset = (trig_angle % (TRIG_MAX_ANGLE * piesize / 180)) / (TRIG_MAX_ANGLE * piesize / 360);
   
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_context_set_stroke_width(ctx, 3);
-  graphics_draw_line(ctx, outer, center);
+  snprintf(debug1, sizeof(debug1), "trig: %d", offset);
+  snprintf(debug2, sizeof(debug2), "pic: %d", coloroffset);
+
   
+  for(int i = -1; i < (180+piesize)/piesize; i++) {
+    if(i % 2) {
+      graphics_context_set_fill_color(ctx, c1);
+    } else {
+      graphics_context_set_fill_color(ctx, c2);
+    }
+    graphics_fill_radial(
+      ctx, 
+      GRect(center.x-radius, center.y-radius, radius*2, radius*2), 
+      GOvalScaleModeFitCircle, 
+      radius, 
+      offset + (i - coloroffset) * DEG_TO_TRIGANGLE(piesize), 
+      offset + (i - coloroffset + 1) * DEG_TO_TRIGANGLE(piesize)
+    );
+  }
+  
+  if(draw_outline) {
+    graphics_context_set_stroke_color(ctx, c2);
+    graphics_draw_circle(ctx, center, radius);
+  }
+}
+
+void draw_disc(GContext *ctx, int trig_angle) {
+  GPoint center = GPoint(-40, window_bounds.size.h/2);
+  GColor c1 = GColorCadetBlue;
+  GColor c2 = GColorMidnightGreen;
+  
+  draw_disc_pies(ctx, trig_angle, 30, 100, center, c1, c2, true);  
+  draw_disc_pies(ctx, trig_angle, 90, 70, center, c1, c2, false);  
+}
+
+void draw_debug_info(GContext *ctx){
   graphics_context_set_text_color(ctx, GColorBlack);
-  graphics_draw_text(ctx, debug1, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(1,1, 144,20), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
-  graphics_draw_text(ctx, debug2, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(1,20, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+  graphics_draw_text(ctx, debug1, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(40,1, 144,20), GTextOverflowModeFill, GTextAlignmentLeft, NULL);
+  graphics_draw_text(ctx, debug2, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(40,20, 144,32), GTextOverflowModeFill, GTextAlignmentLeft, NULL);  
+}
+
+static void canvas_update_proc(Layer *layer, GContext *ctx) {
+  //int trig_angle = rotator();
+  int trig_angle = get_compas_heading();
+  
+  draw_disc(ctx, trig_angle);
+  
+  if(DEBUG) {
+    draw_debug_info(ctx);
+  }
 }
 
 static void timer_callback(void *context) {
